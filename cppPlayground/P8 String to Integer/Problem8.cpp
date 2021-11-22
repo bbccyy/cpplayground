0d8b3540ec9b0b0d5aab040f4e7019eee3b1f576
bbccyy <yw1514@nyu.edu>
2021-11-22 19:22:16 +0800

    add p8

---
+++ cppPlayground/P8 String to Integer/Problem8.cpp
@@ -0,0 +1,18 @@
+#include "Problem8.h"
+#include <stdio.h>
+
+int Problem8::myAtoi(string s)
+{
+
+
+	return 0;
+}
+
+
+void Problem8::runTest()
+{
+	
+	//printf("input = %d, reverse = %d", input, ret);
+
+	return;
+}
---
+++ cppPlayground/P8 String to Integer/Problem8.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Problem8
+{
+public:
+	int myAtoi(string s);
+	void runTest();
+};
+
