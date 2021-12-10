33ca9a00f607cc4ceb6e426f1ecaa209106f7323
bbccyy <yw1514@nyu.edu>
2021-12-11 00:00:02 +0800

    keep work on P28

--- cppPlayground/P028 Implement strStr/Problem28.h
+++ cppPlayground/P028 Implement strStr/Problem28.h
@@ -43,24 +43,44 @@ public:
         {
             j = i;  //start pos
             k = 0;  //new round of matching from beginning 
-            while (k < r_sz && haystack[j] == tbc[k])
+            while (haystack[j] == tbc[k])
             {
                 j2 = j;
-                while (++j2 < sz && haystack[j] == haystack[j2]);
+                while (++j2 < h_sz && haystack[j] == haystack[j2]);
                 h_ct = j2 - j;
                 n_ct = tbn[k];
-                if (h_ct > n_ct)
+                if (h_ct > n_ct)  //not match
                 {
-
+                    if (k == 0)  //is first char, adjust j to right position
+                    {
+                        j = j2 - n_ct;
+                        break;
+                    }
+                    if (tbc[k] != tbc[0])  //not associate to first char, use j2
+                    {
+                        j = j2;
+                        break;
+                    }
+                    break;   //equivalent to first char, use j
                 }
                 else if (h_ct < n_ct)
                 {
-
+                    j = j2;   //no way to match, use j2
+                    break;
                 }
                 j = j2;
                 k++;
+                if (k == r_sz)  //find it!
+                {
+                    return i;
+                }
+                if (j == h_sz)  //search to the end, but not found
+                {
+                    return -1;
+                }
             }
-            
+            if (i == j) ++i;
+            else i = j;
         }
     }
 
