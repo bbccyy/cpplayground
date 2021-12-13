0b43599c47dda6301edc55b5ecd3b6613aa5e616
bbccyy <yw1514@nyu.edu>
2021-12-13 23:06:37 +0800

    add P30

---
+++ cppPlayground/P030 Substring with Concatenation of All Words/Problem30.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Problem30
+{
+public:
+    vector<int> findSubstring(string s, vector<string>& words) {
+
+    }
+
+    void runTest()
+    {
+        
+    }
+};
--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -5,7 +5,7 @@
 
 #include "P010 Reg/Problem10.h"
 //#include "P023 Merge k Sorted Lists/Problem23.h"
-#include "P029 Divide Two Integers/Problem29.h"
+#include "P030 Substring with Concatenation of All Words/Problem30.h"
 
 using namespace std;
 
@@ -14,7 +14,7 @@ int main()
     //auto sln10 = new Problem10();
 	//sln10->runTest();
 
-    auto sln = new Problem29();
+    auto sln = new Problem30();
     sln->runTest();
 	//system("pause");
     return 0;
