fdc1806680c7d394e29bf57a5920e1974c1588cb
bbccyy <yw1514@nyu.edu>
2021-12-15 23:14:16 +0800

    add bs in P031

--- cppPlayground/P031 Next Permutation/Problem31.h
+++ cppPlayground/P031 Next Permutation/Problem31.h
@@ -12,6 +12,26 @@ public:
 
     }
 
+    int binarySearch(vector<int>& nums, int s, int e, int target)
+    {
+        int m = 0;
+        while (s < e)
+        {
+            m = s + (e - s) / 2;
+            if (nums[m] == target)
+                return m;
+            if (nums[m] < target)
+            {
+                e = m - 1;
+            }
+            else
+            {
+                s = m + 1;
+            }
+        }
+        return s;
+    }
+
     void runTest()
     {
         
