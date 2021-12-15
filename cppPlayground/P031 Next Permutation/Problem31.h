b530de6621fb6b0bd926886b3242c4c080d4123e
bbccyy <yw1514@nyu.edu>
2021-12-15 21:58:01 +0800

    init with P031

---
+++ cppPlayground/P031 Next Permutation/Problem31.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Problem31
+{
+public:
+    void nextPermutation(vector<int>& nums) {
+
+    }
+
+    void runTest()
+    {
+        
+    }
+};
--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -6,6 +6,7 @@
 #include "P010 Reg/Problem10.h"
 //#include "P023 Merge k Sorted Lists/Problem23.h"
 #include "P030 Substring with Concatenation of All Words/Problem30.h"
+#include "P031 Next Permutation/Problem31.h"
 
 using namespace std;
 
@@ -14,7 +15,7 @@ int main()
     //auto sln10 = new Problem10();
 	//sln10->runTest();
 
-    auto sln = new Problem30();
+    auto sln = new Problem31();
     sln->runTest();
 	//system("pause");
     return 0;
