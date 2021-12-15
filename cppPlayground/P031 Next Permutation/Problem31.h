b6716acd74412de30cc7d558889b706e755c1518
bbccyy <yw1514@nyu.edu>
2021-12-15 23:29:47 +0800

    deal with P031

--- cppPlayground/P031 Next Permutation/Problem31.h
+++ cppPlayground/P031 Next Permutation/Problem31.h
@@ -9,7 +9,37 @@ class Problem31
 {
 public:
     void nextPermutation(vector<int>& nums) {
+        int sz = nums.size();
+        bool found = false;
+        for (int i = sz - 2; i >= 0; --i)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+                found = true;
+                int j = binarySearch(nums, i + 1, sz - 1, nums[i]);
+                while (nums[j] <= nums[i]) ++j;
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                reverse(nums, i + 1, sz - 1);
+                break;
+            }
+        }
+        if (!found) reverse(nums, 0, sz - 1);
+        return;
+    }
 
+    void reverse(vector<int>& nums, int s, int e)
+    {
+        int tmp = 0;
+        while (s < e)
+        {
+            tmp = nums[s];
+            nums[e] = nums[s];
+            nums[e] = tmp;
+            ++s;
+            --e;
+        }
     }
 
     int binarySearch(vector<int>& nums, int s, int e, int target)
