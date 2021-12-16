120d937bcb37edd05303044399f959176b063fad
bbccyy <yw1514@nyu.edu>
2021-12-17 00:17:20 +0800

    done P32

--- cppPlayground/P032 Longest Valid Parentheses/Problem32.h
+++ cppPlayground/P032 Longest Valid Parentheses/Problem32.h
@@ -11,6 +11,7 @@ public:
     int longestValidParentheses(string s) {
 
         int sz = s.length();
+        int ret = 0;
         vector<vector<bool>> tb = vector<vector<bool>>(sz);
         for (int i= 0; i < sz; ++i)
             tb[i] = vector<bool>(sz);
@@ -27,10 +28,19 @@ public:
                 if (tb[i + 1][j - 1] && s[i] == '(' && s[j] == ')')
                 {
                     tb[i][j] = true;
+                    ret = (j - i) > ret ? (j - i) : ret;
                 }
                 else
                 {
-
+                    for (int p = i + 1; p < j - 1; p += 2)
+                    {
+                        if (tb[i][p] && tb[p + 1][j])
+                        {
+                            tb[i][j] = true;
+                            ret = (j - i) > ret ? (j - i) : ret;
+                            break;
+                        }
+                    }
                 }
 
                 //inc
@@ -39,6 +49,7 @@ public:
             }
         }
 
+        return ret == 0 ? 0 : ret + 1;
     }
 
     void runTest()
