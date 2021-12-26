8c325b9636a59eb627c4c4033f9448ab554d6a7e
bbccyy <yw1514@nyu.edu>
2021-12-26 13:53:31 +0800

    add p40

---
+++ cppPlayground/P040 Combination Sum II/Problem40.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Problem40
+{
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+
+    }
+
+    void runTest()
+    {
+        
+    }
+};
