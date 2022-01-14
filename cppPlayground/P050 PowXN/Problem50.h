bce0579dcd6a8544715e77054c45f9d4f2d1e670
bbccyy <yw1514@nyu.edu>
2022-01-14 11:34:21 +0800

    done P50

--- cppPlayground/P050 PowXN/Problem50.h
+++ cppPlayground/P050 PowXN/Problem50.h
@@ -9,10 +9,28 @@ class Problem50
 {
 public:
 	double myPow(double x, int n) {
-
+		return n > 0 ? calPow(x, n) : calPow(1.0 / x, 0-((long)n));
 	}
+
+	double calPow(double x, long n)
+	{
+		double ret = 1;
+		while (n > 0)
+		{
+			if (n & 1) //last digit is 1
+			{
+				ret *= x;
+			}
+			x *= x;
+			n = n >> 1;
+		}
+		return ret;
+	}
+
 	void runTest()
 	{
-		
+		auto ret = myPow(10, 4);
+
+		cout << "ret = " << ret << endl;
 	}
 };
