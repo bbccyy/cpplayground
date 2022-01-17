66453190150771b022e089f372b7c61b32d90289
bbccyy <yw1514@nyu.edu>
2022-01-17 13:44:13 +0800

    add p051

---
+++ cppPlayground/P051 N-Queens/Problem51.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Problem51
+{
+public:
+	vector<vector<string>> solveNQueens(int n) {
+
+	}
+
+	void runTest()
+	{
+		
+	}
+};
