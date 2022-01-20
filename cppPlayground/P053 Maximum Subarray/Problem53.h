63ba25b034eafd2ed320f050866efa983d2f56be
bbccyy <yw1514@nyu.edu>
2022-01-20 10:58:43 +0800

    done with p53

--- cppPlayground/P053 Maximum Subarray/Problem53.h
+++ cppPlayground/P053 Maximum Subarray/Problem53.h
@@ -10,11 +10,23 @@ class Problem53
 {
 public:
 	int maxSubArray(vector<int>& nums) {
-
+		int sz = size(nums);
+		int p = 0, sum = 0, max = INT_MIN;
+		while (p < sz)
+		{
+			sum += nums[p++];
+			if (max < sum) max = sum;
+			if (sum < 0) sum = 0;
+		}
+		return max;
 	}
 
 	void runTest()
 	{
-		
+		vector<int> input = { -2,1,-3,4,-1,2,1,-5,4 };//[-2,1,-3,4,-1,2,1,-5,4]
+
+		auto ret = maxSubArray(input);
+
+		cout << "ret = " << ret << endl;
 	}
 };
