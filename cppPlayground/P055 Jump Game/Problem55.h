cd79803377498e33ad3cfba0ff58afd1c2c7ca5a
bbccyy <yw1514@nyu.edu>
2022-01-24 18:09:09 +0800

    done with P055

--- cppPlayground/P055 Jump Game/Problem55.h
+++ cppPlayground/P055 Jump Game/Problem55.h
@@ -19,11 +19,30 @@ class Problem55
 {
 public:
 	bool canJump(vector<int>& nums) {
-
+		int sz = size(nums);
+		if (sz <= 0) return true;
+		int p = 0, d = nums[p], boundary = 0, tmp = 0;
+		while (p + d < sz - 1)
+		{
+			if (d == 0) return false;
+			boundary = p;
+			for (int i = 1; i <= d; ++i)
+			{
+				tmp = p + i + nums[p + i];
+				boundary = boundary > tmp ? boundary : tmp;
+			}
+			p = p + d;
+			d = boundary - p;
+		}
+		return true;
 	}
 
 	void runTest()
 	{
-		
+		vector<int> input = { 3,2,1,0,4 };
+
+		auto ret = canJump(input);
+
+		cout << "ret = " << ret << endl;
 	}
 };
