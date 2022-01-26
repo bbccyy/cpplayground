36bf884a4583b6d09ac39ac5213bae94aa3d21d4
bbccyy <yw1514@nyu.edu>
2022-01-26 18:27:17 +0800

    deal with P056

--- cppPlayground/P056 Merge Intervals/Problem56.h
+++ cppPlayground/P056 Merge Intervals/Problem56.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -18,12 +19,52 @@ Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 class Problem56
 {
 public:
-	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+	static bool compare(vector<int>& a, vector<int>& b)
+	{
+		return a[0] < b[0];
+	}
 
+	vector<vector<int>> merge(vector<vector<int>>& intervals) {
+		int sz = size(intervals);
+		if (sz <= 1) return intervals;
+
+		vector<vector<int>> output;
+		sort(intervals.begin(), intervals.end(), compare);
+		int s = intervals[0][0], e = intervals[0][1]; 
+		for (int i = 1; i < sz; ++i)
+		{
+			auto& cur = intervals[i];
+			if (cur[0] > e)  //no interval
+			{
+				output.emplace_back(vector<int>{s, e});
+				s = cur[0];
+				e = cur[1];
+			}
+			else  //has interval
+			{
+				e = cur[1] > e ? cur[1] : e;  //try refresh the end value 
+			}
+		}
+		output.emplace_back(vector<int>{s, e});
+
+		return output;
 	}
 
 	void runTest()
 	{
-		
+		vector<vector<int>> input = { {1,3},{2,6},{8,10},{15,18} };
+
+		auto ret = merge(input);
+
+		cout << "ret = " << endl;
+		for (int i = 0; i < ret.size(); ++i)
+		{
+			cout << "[";
+			for (int j = 0; j < ret[i].size(); ++j)
+			{
+				cout << ret[i][j] << " ";
+			}
+			cout << "]" << endl;
+		}
 	}
 };
