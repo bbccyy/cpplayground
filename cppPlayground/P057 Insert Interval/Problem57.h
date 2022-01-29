e8b3d9a8dd0b400fd76599e6b1892ef717b66b20
bbccyy <yw1514@nyu.edu>
2022-01-29 19:07:50 +0800

    add idea to p057

--- cppPlayground/P057 Insert Interval/Problem57.h
+++ cppPlayground/P057 Insert Interval/Problem57.h
@@ -21,7 +21,9 @@ class Problem57
 {
 public:
 	vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-
+		//binary search start point: origin = [[2,5][7,9] ...], new = [3,e] -> find the greatest start point that less or equal than 3, which is [2,5]
+		//binary search end point: origin = [... [21,30][31,33]], new = [17, 31] -> find the smallest end point that greater or equals than 31, which is [31,33]
+		//merge if necessary 
 	}
 
 	void runTest()
