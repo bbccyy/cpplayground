8ae8a7adadb205915a93a50d8b72360056621208
bbccyy <yw1514@nyu.edu>
2022-02-10 00:03:34 +0800

    rm some includes

--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -3,25 +3,6 @@
 #include <string>
 #include <vector>
 
-#include "P010 Reg/Problem10.h"
-//#include "P023 Merge k Sorted Lists/Problem23.h"
-#include "P030 Substring with Concatenation of All Words/Problem30.h"
-#include "P031 Next Permutation/Problem31.h"
-#include "P032 Longest Valid Parentheses/Problem32.h"
-#include "P033 Search in Rotated Sorted Array/Problem33.h"
-#include "P034 Find First and Last Position of Element in Sorted Array/Problem34.h"
-#include "P036 Valid Sudoku/Problem36.h"
-//#include "P037 Sudoku Solver/Problem37.h"
-#include "P038 Count and Say/Problem38.h"
-#include "P041 First Missing Positive/Problem41.h"
-#include "P042 Trapping Rain Water/Problem42.h"
-#include "P043 Multiply Strings/Problem43.h"
-#include "P044 Wildcard Matching/Problem44.h"
-#include "P045 Jump Game II/Problem45.h"
-#include "P047 Permutations II/Problem47.h"
-#include "P049 Group Anagrams/Problem49.h"
-#include "P054 Spiral Matrix/Problem54.h"
-#include "P055 Jump Game/Problem55.h"
 #include "P058 Length of Last Word/Problem58.h"
 
 
