e521b4cde77b8123aa2f3c383c180d65b1702cc1
bbccyy <yw1514@nyu.edu>
2022-02-13 14:29:02 +0800

    add 60

--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include "P059 Spiral Matrix II/Problem59.h"
+#include "P060 Permutation Sequance/Problem60.h"
 
 
 using namespace std;
@@ -13,7 +14,7 @@ int main()
     //auto sln10 = new Problem10();
 	//sln10->runTest();
 
-    auto sln = new Problem59();
+    auto sln = new Problem60();
     sln->runTest();
 	//system("pause");
     return 0;
