c345490ecf0d6f6855c3eff5c565ce2320741fc3
bbccyy <yw1514@nyu.edu>
2022-02-16 10:11:34 +0800

    add p61

---
+++ cppPlayground/P061 Rotate List/Problem61.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+/*
+Given the head of a linked list, rotate the list to the right by k places.
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+*/
+class Problem61
+{
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+
+    }
+
+    void runTest()
+    {
+
+    }
+};
--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -5,6 +5,7 @@
 
 #include "P059 Spiral Matrix II/Problem59.h"
 #include "P060 Permutation Sequance/Problem60.h"
+#include "P061 Rotate List/Problem61.h"
 
 
 using namespace std;
@@ -14,7 +15,7 @@ int main()
     //auto sln10 = new Problem10();
 	//sln10->runTest();
 
-    auto sln = new Problem60();
+    auto sln = new Problem61();
     sln->runTest();
 	//system("pause");
     return 0;
