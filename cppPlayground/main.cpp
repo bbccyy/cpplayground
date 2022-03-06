bce522ed668909c5b9772ae619de31f187008b28
bbccyy <yw1514@nyu.edu>
2022-03-06 09:44:32 +0800

    add p65

--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -6,8 +6,7 @@
 #include "P059 Spiral Matrix II/Problem59.h"
 #include "P060 Permutation Sequance/Problem60.h"
 #include "P061 Rotate List/Problem61.h"
-#include "P063 Unique Paths II/Problem63.h"
-#include "p064 Minimum Path Sum/Problem64.h"
+#include "P65 Valid Number/Problem65.h"
 
 
 using namespace std;
@@ -17,7 +16,7 @@ int main()
     //auto sln10 = new Problem10();
 	//sln10->runTest();
 
-    auto sln = new Problem64();
+    auto sln = new Problem65();
     sln->runTest();
 	//system("pause");
     return 0;
