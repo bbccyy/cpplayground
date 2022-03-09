6d3b3180060ad39cb1fa7bf07bce6d1b2d4b2da2
bbccyy <yw1514@nyu.edu>
2022-03-09 11:32:58 +0800

    ok with p67

--- cppPlayground/P067 Add Binary/Problem67.h
+++ cppPlayground/P067 Add Binary/Problem67.h
@@ -2,6 +2,9 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
+#include <strstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -19,11 +22,51 @@ class Problem67
 {
 public:
     string addBinary(string a, string b) {
-
+        strstream ss;
+        string dummy;
+        string& max = dummy, min = dummy;
+        if (a.size() < b.size())
+        {
+            max = b;
+            min = a;
+        }
+        else {
+            max = a;
+            min = b;
+        }
+        int carry = 0, tmp = 0, i = max.size() - 1;
+        for (int j = min.size() - 1; j >= 0; --i, --j)
+        {
+            tmp = (max[i] - '0') + (min[j] - '0') + carry;
+            carry = tmp > 1 ? 1 : 0;
+            tmp = tmp - carry * 2;
+            ss << tmp;
+        }
+        while (i >= 0)
+        {
+            tmp = (max[i] - '0') + carry;
+            carry = tmp > 1 ? 1 : 0;
+            tmp = tmp - carry * 2;
+            ss << tmp;
+            --i;
+        }
+        if (carry > 0)
+        {
+            ss << 1;
+        }
+        ss << '\0';
+        string output = ss.str();
+        reverse(output.begin(), output.end());
+        return output;
     }
 
     void runTest()
     {
+        string inputA = "1";
+        string inputB = "11";
 
+        auto ret = addBinary(inputA, inputB);
+
+        cout << "ret = " << ret << endl;
     }
 };
