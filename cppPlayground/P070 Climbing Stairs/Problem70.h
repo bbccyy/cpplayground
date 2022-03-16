329f84c4d11f3a7b2b4d28b32535e0726bc97ee3
bbccyy <yw1514@nyu.edu>
2022-03-16 14:59:05 +0800

    add 70

---
+++ cppPlayground/P070 Climbing Stairs/Problem70.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Problem70
+{
+public:
+    int climbStairs(int n) {
+
+    }
+
+    void runTest()
+    {
+
+    }
+};
--- cppPlayground/main.cpp
+++ cppPlayground/main.cpp
@@ -9,7 +9,7 @@
 #include "P65 Valid Number/Problem65.h"
 #include "P067 Add Binary/Problem67.h"
 #include "P068 Text Justification/Problem68.h"
-#include "P069 SqrtX/Problem69.h"
+#include "P070 Climbing Stairs/Problem70.h"
 
 
 using namespace std;
@@ -19,7 +19,7 @@ int main()
     //auto sln10 = new Problem10();
 	//sln10->runTest();
 
-    auto sln = new Problem69();
+    auto sln = new Problem70();
     sln->runTest();
 	//system("pause");
     return 0;
