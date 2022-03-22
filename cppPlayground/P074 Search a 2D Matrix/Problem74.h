731eff4607244a58fc37b163cd24e8dfc5b9df00
bbccyy <yw1514@nyu.edu>
2022-03-22 22:04:06 +0800

    mostly ok with p074

--- cppPlayground/P074 Search a 2D Matrix/Problem74.h
+++ cppPlayground/P074 Search a 2D Matrix/Problem74.h
@@ -15,11 +15,52 @@ class Problem74
 {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //two binary search!
+        int r_sz = matrix.size();
+        int c_sz = matrix[0].size();
 
+        if (matrix[0][0] > target || matrix[r_sz - 1][c_sz - 1] < target)
+            return false;
+
+        int s = 0, e = r_sz - 1, m = 0;
+        while (s < e)
+        {
+            m = s + (e - s) / 2;
+            if (matrix[m][0] == target) return true;
+            if (matrix[m][0] < target)
+            {
+                s = m + 1;
+            }
+            else
+            {
+                e = m - 1;
+            }
+        }
+
+        int r = e;
+        if (r < 0) r = 0;
+        s = 0, e = c_sz - 1;
+        while (s < e)
+        {
+            m = s + (e - s) / 2;
+            if (matrix[r][m] == target) return true;
+            if (matrix[r][m] < target)
+            {
+                s = m + 1;
+            }
+            else
+            {
+                e = m - 1;
+            }
+        }
+        if (e < 0) e = 0;
+
+        
+        return  matrix[r][e] == target;
     }
 
     void runTest()
     {
-
+        //todo 
     }
 };
