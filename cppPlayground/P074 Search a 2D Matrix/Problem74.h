b7a0bca05e382e20d1bb44d252ac603a8279b712
bbccyy <yw1514@nyu.edu>
2022-03-23 10:28:53 +0800

    mostly ok with p074 2

--- cppPlayground/P074 Search a 2D Matrix/Problem74.h
+++ cppPlayground/P074 Search a 2D Matrix/Problem74.h
@@ -59,8 +59,68 @@ public:
         return  matrix[r][e] == target;
     }
 
+    bool searchMatrix2(vector<vector<int>>& m, int t) {
+        // pointless edge case we still have to consider
+        if (!m.size() || !m[0].size()) return false;
+        // support variables
+        int row, l = 0, r = m.size() - 1, mid;
+        while (l < r) {
+            mid = (l + r) / 2;
+            // moving l up if needed
+            if (m[mid].back() < t) l = mid + 1;
+            // moving r down if needed
+            else if (m[mid][0] > t) r = mid - 1;
+            // we found our row!
+            else {
+                l = mid;
+                break;
+            };
+        }
+        // storing the value of the new found row
+        row = l;
+        // resetting l and r to run a binary search on the rows
+        l = 0;
+        r = m[0].size() - 1;
+        while (l <= r) {
+            mid = (l + r) / 2;
+            // moving l up if needed
+            if (m[row][mid] < t) l = mid + 1;
+            // moving r down if needed
+            else if (m[row][mid] > t) r = mid - 1;
+            // we found our value!
+            else return true;
+        }
+        return false;
+    }
+
     void runTest()
     {
         //todo 
     }
 };
+
+/*
+
+Alright: all the elements are sorted and we need to try and find a specific element, right? That is a no-brainer: we need to go with a binary search approach.
+
+Provided neither the matrix or the rows are empty, since a nice problem like this needs to get burdened by pointless requirements that boil down to the chore of writing an extra line to remove silly edge cases <_<
+
+Now, the relatively tricky part here is that we do not have all of them in a single flat container, but in a matrix. We will actually then want to do 2 binary searches: one to find the row to be searched, one to search it directly.
+
+To do so, I declared 4 variables:
+
+row, meant to store which row will be searched, once we found one;
+l, r will store the extremes of our search, initialised to 0 and m.size() - 1, respectively;
+mid will store the on going average of l and r.
+First round of binary search:
+
+we go on until l < r [note that the first one has to be a strict comparison, unlike for the second search];
+at each iteration we will compute mid as (l + r) / 2;
+we will then compare and find that if t is bigger than the last element of the row we are checking (m[mid].back() < t), it is time to raise l up to mid + 1 and loop again;
+specularly, if t is smaller than the first element of the row we are checking (m[mid][0] > t), it is time to lower r down to mid - 1 and loop again;
+finally, if neithere condition is correct, we it means that m[mid][0] <= t && m[mid].back() >= t, which implies we are already checking the right row, so we just assign l = mid and break out of the loop.
+Be as it is, once we are done iterating, we will store the value of the correct row to search in row, reset l and r and move on with a similar logic to parse this time on a more traditional way if the element is in the row or not: a match will have us return true.
+
+If we exit the loop without any joy, we return false.
+
+*/
