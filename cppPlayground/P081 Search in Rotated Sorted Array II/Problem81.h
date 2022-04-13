d195d5d13c20058e96db757795a6be11a61647ae
bbccyy <yw1514@nyu.edu>
2022-04-13 10:47:01 +0800

    still 81

--- cppPlayground/P081 Search in Rotated Sorted Array II/Problem81.h
+++ cppPlayground/P081 Search in Rotated Sorted Array II/Problem81.h
@@ -69,7 +69,7 @@ public:
     {
         int sz = nums.size();
         int s = 0, e = sz - 1, m = 0;
-        while (s < e)
+        while (s < e -1)
         {
             m = s + (e - s) / 2;  //m tends to s 
             if (nums[s] < nums[m])
@@ -99,9 +99,54 @@ public:
         return s;
     }
 
+
+    bool search2(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l <= r)
+        {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return true;
+            // with duplicates we can have this contdition, just update left & right
+            if ((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
+            {
+                l++;
+                r--;
+            }
+            // first half
+            // first half is in order
+            else if (nums[l] <= nums[mid])
+            {
+                // target is in first  half
+                if ((nums[l] <= target) && (nums[mid] > target))
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            // second half
+            // second half is order
+            // target is in second half
+            else
+            {
+                if ((nums[mid] < target) && (nums[r] >= target))
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+        }
+        return false;
+    }
+
+
     void runTest()
     {
-        //TODO
+        vector<int> input = { 5,5,5,5,5,5,5,2,5,5 };
+
+        auto ret = findPivot(input);
+
+        cout << "ret = " << ret << endl;
 
     }
 };
