0693dcca430ff31762abc559bb1a1dab211ab0be
bbccyy <yw1514@nyu.edu>
2022-04-26 15:00:41 +0800

    add idea to p85

--- cppPlayground/P085 Maximal Rectangle/Problem85.h
+++ cppPlayground/P085 Maximal Rectangle/Problem85.h
@@ -33,8 +33,41 @@ matrix[i][j] is '0' or '1'.
 class Problem85
 {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
+    int max(int a, int b)
+    {
+        return a > b ? a : b;
+    }
 
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int r_sz = matrix.size();
+        if (r_sz <= 0) return 0;
+        int c_sz = matrix[0].size();
+        
+        vector<int> tb = vector<int>(c_sz, 0);
+        for (int c = 0; c < c_sz; ++c)
+            tb[c] = matrix[0][c] == '0' ? 0 : 1;
+
+        int ret = maximalRectangleOnLine(tb);
+
+        for (int r = 1; r < r_sz; ++r)
+        {
+            for (int c = 0; c < c_sz; ++c)
+            {
+                if (matrix[r][c] == '1')
+                    ++tb[c];
+                else
+                    tb[c] = 0;
+            }
+            ret = max(ret, maximalRectangleOnLine(tb));
+        }
+        
+        return ret;
+    }
+
+    int maximalRectangleOnLine(vector<int> input)
+    {
+
+        return 0;
     }
 
     void runTest()
